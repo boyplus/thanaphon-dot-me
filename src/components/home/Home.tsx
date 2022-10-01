9eeccd033301de311d8f2121a2cfd3a8d3c11657
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2022-10-01 18:05:31 +0700

    update: home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,11 +4,27 @@ import './home.css';
 const Home: React.FC = () => {
   return (
     <main className='home'>
-      <img src="/images/profile.jpg" width="250px" height="333px"></img>
+      <img src="/images/profile.jpg" width="250px" height="333px" className='profile-pic'></img>
       <div className="content">
-        <h1>Thanaphon Sombunkaeo</h1>
+        <h1 className='blue'>Thanaphon Sombunkaeo (Boy)</h1>
+        <p>"A person who loves problem solving and full-stack development."</p>
+        <div className='experiences'>
+          <h3>🔥 Experiences (in brief)</h3>
+          <ul>
+            <li>Sofrware Engineer (full-stack) intern at Agoda (Bangkok)</li>
+            <li>Freelance full-stack web developer @ KKP Cargo{'&'}Logistics</li>
+            <li>Content creator of GDSC KMUTT (Google Developer Student Club)</li>
+          </ul>
+        </div>
+        <div className='talk'>
+          <h3>🎙 Talk to me about</h3>
+          <ul>
+            <li>Algorithms and full-stack development</li>
+            <li>KPOP (boy groups)</li>
+            <li>Books</li>
+          </ul>
+        </div>
       </div>
-
     </main>
   )
 }
