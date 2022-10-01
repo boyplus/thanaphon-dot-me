0dd76eb84587faa097db1334767b68f84e3216ce
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2022-10-01 22:41:12 +0700

    update: favicon

---
+++ src/components/about/Educations.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface IEducation {
+  place: string;
+  year: string;
+  description: string;
+  gpax: string;
+  certificate: string;
+}
+
+const educations = [];
+
+const Educations: React.FC = () => {
+  return <div></div>
+}
+
+export default Educations;
