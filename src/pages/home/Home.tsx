938cfebaf2d306d697b3155e0e932ba9a4e9443d
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2022-10-01 18:42:06 +0700

    add: content list

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import './home.css';
 
+const contents = [
+  {
+    header: '🔥 Experiences (in brief)', items: [
+      'Sofrware Engineer (full-stack) intern at Agoda (Bangkok)',
+      'Freelance full-stack web developer @ KKP Cargo&Logistics',
+      'Content creator of GDSC KMUTT (Google Developer Student Club)'
+    ]
+  },
+  {
+    header: '🎙 Talk to me about', items: [
+      'KPOP (boy groups)',
+      'Algorithms',
+      'Web development'
+    ]
+  }
+];
+
 const Home: React.FC = () => {
+
+  const contentList = contents.map(content => {
+    return <div className='content-container'>
+      <h3>{content.header}</h3>
+      <ul>
+        {content.items.map(item => <li>{item}</li>)}
+      </ul>
+    </div>
+  });
+
   return (
     <main className='home'>
       <img src="/images/profile.jpg" width="250px" height="333px" className='profile-pic'></img>
-      <div className="content">
+      <div className="profile">
         <h1 className='blue'>Thanaphon Sombunkaeo (Boy)</h1>
         <p>"A person who loves problem solving and full-stack development."</p>
-        <div className='experiences'>
-          <h3>🔥 Experiences (in brief)</h3>
-          <ul>
-            <li>Sofrware Engineer (full-stack) intern at Agoda (Bangkok)</li>
-            <li>Freelance full-stack web developer @ KKP Cargo{'&'}Logistics</li>
-            <li>Content creator of GDSC KMUTT (Google Developer Student Club)</li>
-          </ul>
-        </div>
-        <div className='talk'>
-          <h3>🎙 Talk to me about</h3>
-          <ul>
-            <li>KPOP (boy groups)</li>
-            <li>Algorithms</li>
-            <li>Web development</li>
-          </ul>
+        <div className='content-list-container'>
+          {contentList}
         </div>
       </div>
     </main>
