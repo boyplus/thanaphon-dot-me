f20dd41c15858ed9cebded37ea71d666430745cf
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2022-10-09 18:08:33 +0700

    add: responsive to about page

--- src/components/about/bio/Bio.tsx
+++ src/components/about/bio/Bio.tsx
@@ -4,15 +4,18 @@ import './bio.css';
 const Bio: React.FC = () => {
   return (
     <div className='bio'>
-      <img
-        src="/images/profile-square-2.jpeg"
-        width="200px"
-        height="202px"
-        className='profile-pic'
-        alt="Profile picture"
-      />
+      <div className='profile-pic-container'>
+        <img
+          src="/images/profile-square-2.jpeg"
+          width="200px"
+          height="202px"
+          className='profile-pic'
+          alt="Profile picture"
+        />
+      </div>
+
       <div className='short-bio'>
-        <h1 className='blue'>Thanaphon Sombunkaeo (Boy)</h1>
+        <h1 className='blue fit-content name'>Thanaphon Sombunkaeo (Boy)</h1>
         <div className='position'>
           <div className='line'></div>
           <h3 className='grey'>Full-stack software developer</h3>
