d735c491a31e8bde0cc999137bb3d4bcf0ad44c2
boyplus <ict224bj@gmail.com>
2023-02-10 01:19:21 +0700

    fix: navbar

--- src/components/navbar/NavbarItem.tsx
+++ src/components/navbar/NavbarItem.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 
@@ -10,7 +10,8 @@ export interface INavbarItem {
 
 const NavbarItem: React.FC<INavbarItem> = ({ label, link, page }) => {
   const location = useLocation();
-  const isActive = location.pathname === link;
+  const isActive = link === '/' ? location.pathname === '/' : location.pathname.includes(link);
+
   return (
     <li className={isActive ? 'active' : ''}>
       <div className="dot"></div>
